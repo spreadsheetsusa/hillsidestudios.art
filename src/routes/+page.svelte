2e85b70014cf5f79f3711526426431aecab3106f
Benjamin Keating <ben@abstractly.io>
2023-09-17 10:04:41 -0500

    sp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center w-screen h-screen justify-center">
+  <div>
+    <img src="IMG_2026.jpeg" alt="Evvy & Dad <3"  class="rounded-2xl shadow-2xl border border-gray-400" />
+    <h1 class="mt-6 text-center text-2xl">Hillside Studios</h1>
+  </div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
